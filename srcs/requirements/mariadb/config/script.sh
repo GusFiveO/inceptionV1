b50d22d58bce26836c86df18714d5c55df8e262d
root <root@alorain.alorain>
2022-07-29 13:50:53 +0200

    script modif

--- srcs/requirements/mariadb/config/script.sh
+++ srcs/requirements/mariadb/config/script.sh
@@ -3,6 +3,7 @@
 echo "creating DB..."
 envsubst < /var/init.sql > /var/init_env.sql
 service mysql start
-mysql -D mysql < /var/init_env.sql
+mysql -D mysql < /var/init_env.sql | true
 touch /var/lib/mysql/.db_create
-service mysql stop
+service mysql stop | echo -n ""
+exec mysqld_safe
